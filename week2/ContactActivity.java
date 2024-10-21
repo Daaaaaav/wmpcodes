5fc9dc7c5f3ec11f58898a94ff9059c16e2c7df1
Davina Ritzky Amarina <144467455+Daaaaaav@users.noreply.github.com>
2024-10-21 22:51:02 +0700

    Create ContactActivity.java

---
+++ week2/ContactActivity.java
@@ -0,0 +1,31 @@
+package com.example.week2;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ContactActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_contact);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void sendEmail(View view) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto:scarletstormsubs@gmail.com"));
+        startActivity(Intent.createChooser(emailIntent, "Send email..."));
+    }
+}
