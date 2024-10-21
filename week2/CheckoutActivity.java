1cb600737e2b41c079ef3cfe45d52846a359ee7e
Davina Ritzky Amarina <144467455+Daaaaaav@users.noreply.github.com>
2024-10-21 22:50:14 +0700

    Create CheckoutActivity.java

---
+++ week2/CheckoutActivity.java
@@ -0,0 +1,29 @@
+package com.example.week2;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CheckoutActivity extends AppCompatActivity {
+    TextView textDetails;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_checkout);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textDetails = findViewById(R.id.textDetails);
+        String productName = getIntent().getStringExtra("Product_Name");
+        String quantity = getIntent().getStringExtra("Quantity");
+        textDetails.setText("Product: " + productName + "\nQuantity: " + quantity);
+    }
+}
